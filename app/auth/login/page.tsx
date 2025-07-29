c85b3da1826c307f3bd118caadedfa2aea1a5367
Siyabonga Mbuyisa <siyabongambuyisa7@gmail.com>
2025-07-29 14:15:59 +0200

    SignIn & SignUp function Added

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,11 +1,22 @@
 import { LoginForm } from "@/components/login-form";
-
+const { createClient } = require("@supabase/supabase-js");
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_OR_ANON_KEY;
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
 export default function Page() {
-  return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm">
-        <LoginForm />
-      </div>
-    </div>
-  );
+  // return (
+  //   <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+  //     <div className="w-full max-w-sm">
+  //       <LoginForm />
+  //     </div>
+  //   </div>
+  // );
+  signIn: async (email:string, password:string) => {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password
+    });
+    return { data, error };
+  
+  }
 }
--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -1,11 +1,30 @@
 import { SignUpForm } from "@/components/sign-up-form";
+const { createClient } = require("@supabase/supabase-js");
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_OR_ANON_KEY;
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default function Page() {
-  return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm">
-        <SignUpForm />
-      </div>
-    </div>
-  );
+  
+    // <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+    //   <div className="w-full max-w-sm">
+    //     <SignUpForm />
+    //   </div>
+    // </div>
+    signUp: async (email:string, password:string, name:string, surname:string, phone_number:string, profile_picture:string) => {
+    const { data, error } = await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        data: {
+          Name: name,
+          Surname: surname,
+          Phone_Number: phone_number,
+          Profile_Picture: profile_picture
+          
+        }
+      }
+    });
+    return { data, error };
+  }
 }
