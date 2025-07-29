00296ffd25e44a688e9ee0cb1d0016a1acfcd079
Siyabonga Mbuyisa <siyabongambuyisa7@gmail.com>
2025-07-29 14:38:21 +0200

    Forgot Password Added

--- app/auth/forgot-password/page.tsx
+++ app/auth/forgot-password/page.tsx
@@ -1,11 +1,22 @@
 import { ForgotPasswordForm } from "@/components/forgot-password-form";
+import { SignUpForm } from "@/components/sign-up-form";
+const { createClient } = require("@supabase/supabase-js");
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_OR_ANON_KEY;
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+
 
 export default function Page() {
-  return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm">
-        <ForgotPasswordForm />
-      </div>
-    </div>
-  );
+  // return (
+  //   <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+  //     <div className="w-full max-w-sm">
+  //       <ForgotPasswordForm />
+  //     </div>
+  //   </div>
+  // );
+  forgotPassword: async (email: string) => {
+    const { data, error } = await supabase.auth.resetPasswordForEmail(email);
+    return { data, error };
+  }
 }
